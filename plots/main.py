11babf6ef2d812036a4fded7bf7b1c142197bc63
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-01-14 00:37:09 +0900

    2018.01.14-00:37

--- plots/main.py
+++ plots/main.py
@@ -6,167 +6,40 @@ import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
-def l2fwd1b_delay000_pkt064():
-    inputname  = 'dat_l2fwd1b_delay000_pkt064.dat'
-    outputname = 'delay000_pkt064'
+
+def draw_fig(delay, pktsize, f_idx, t1_idx, t2_idx, t4_idx):
+    inputname  = 'sinewave_traffic.csv'
+    outputname = 'delay{:03}_pkt{:03}'.format(delay, pktsize)
     print('{}(in={}, out={})'.format(
         sys._getframe().f_code.co_name,
         inputname, outputname))
 
     data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=64Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=0")
-    plt.plot(input3, label="TPR #thrd=2, delay=0")
-    plt.plot(input4, label="TPR #thrd=4, delay=0")
+    flow  = data[:,f_idx]
+    thrd1 = data[:,t1_idx]
+    thrd2 = data[:,t2_idx]
+    thrd4 = data[:,t4_idx]
+
+    plt.ylim([0,105])
+    plt.plot(flow, label="Traffic pktsize={}Byte".format(pktsize))
+    plt.plot(thrd1, label="TPR #thrd=1, delay={}".format(delay))
+    plt.plot(thrd2, label="TPR #thrd=2, delay={}".format(delay))
+    plt.plot(thrd4, label="TPR #thrd=4, delay={}".format(delay))
     plt.legend(loc=2)
     plt.savefig(outputname)
+    plt.close()
 
-def l2fwd1b_delay000_pkt128():
-    inputname  = 'dat_l2fwd1b_delay000_pkt128.dat'
-    outputname = 'delay000_pkt128'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=128Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=0")
-    plt.plot(input3, label="TPR #thrd=2, delay=0")
-    plt.plot(input4, label="TPR #thrd=4, delay=0")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay020_pkt064():
-    inputname  = 'dat_l2fwd1b_delay020_pkt064.dat'
-    outputname = 'delay020_pkt064'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=64Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=20")
-    plt.plot(input3, label="TPR #thrd=2, delay=20")
-    plt.plot(input4, label="TPR #thrd=4, delay=20")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay020_pkt128():
-    inputname  = 'dat_l2fwd1b_delay020_pkt128.dat'
-    outputname = 'delay020_pkt128'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=128Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=20")
-    plt.plot(input3, label="TPR #thrd=2, delay=20")
-    plt.plot(input4, label="TPR #thrd=4, delay=20")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay050_pkt064():
-    inputname  = 'dat_l2fwd1b_delay050_pkt064.dat'
-    outputname = 'delay050_pkt064'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=64Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=50")
-    plt.plot(input3, label="TPR #thrd=2, delay=50")
-    plt.plot(input4, label="TPR #thrd=4, delay=50")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay050_pkt128():
-    inputname  = 'dat_l2fwd1b_delay050_pkt128.dat'
-    outputname = 'delay050_pkt128'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=128Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=50")
-    plt.plot(input3, label="TPR #thrd=2, delay=50")
-    plt.plot(input4, label="TPR #thrd=4, delay=50")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay100_pkt064():
-    inputname  = 'dat_l2fwd1b_delay100_pkt064.dat'
-    outputname = 'delay100_pkt064'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=64Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=100")
-    plt.plot(input3, label="TPR #thrd=2, delay=100")
-    plt.plot(input4, label="TPR #thrd=4, delay=100")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
-
-def l2fwd1b_delay100_pkt128():
-    inputname  = 'dat_l2fwd1b_delay100_pkt128.dat'
-    outputname = 'delay100_pkt128'
-    print('{}(in={}, out={})'.format(
-        sys._getframe().f_code.co_name,
-        inputname, outputname))
-
-    data = np.loadtxt(inputname, delimiter=',')
-    input1 = data[:,1]
-    input2 = data[:,2]
-    input3 = data[:,3]
-    input4 = data[:,4]
-    plt.plot(input1, label="Traffic pktsize=128Byte")
-    plt.plot(input2, label="TPR #thrd=1, delay=100")
-    plt.plot(input3, label="TPR #thrd=2, delay=100")
-    plt.plot(input4, label="TPR #thrd=4, delay=100")
-    plt.legend(loc=2)
-    plt.savefig(outputname)
 
 def main():
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay000_pkt064()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay000_pkt128()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay020_pkt064()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay020_pkt128()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay050_pkt064()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay050_pkt128()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay100_pkt064()
-    plt.close(); plt.ylim([0,105]); l2fwd1b_delay100_pkt128()
+    draw_fig(delay=0  , pktsize=64 , f_idx=1, t1_idx=2 , t2_idx=3 , t4_idx=4 )
+    draw_fig(delay=0  , pktsize=128, f_idx=1, t1_idx=5 , t2_idx=6 , t4_idx=7 )
+    draw_fig(delay=20 , pktsize=64 , f_idx=1, t1_idx=8 , t2_idx=9 , t4_idx=10)
+    draw_fig(delay=20 , pktsize=128, f_idx=1, t1_idx=11, t2_idx=12, t4_idx=13)
+    draw_fig(delay=50 , pktsize=64 , f_idx=1, t1_idx=14, t2_idx=15, t4_idx=16)
+    draw_fig(delay=50 , pktsize=128, f_idx=1, t1_idx=17, t2_idx=18, t4_idx=19)
+    draw_fig(delay=100, pktsize=64 , f_idx=1, t1_idx=20, t2_idx=21, t4_idx=22)
+    draw_fig(delay=100, pktsize=128, f_idx=1, t1_idx=23, t2_idx=24, t4_idx=25)
+
 
 if __name__ == '__main__':
     main()
